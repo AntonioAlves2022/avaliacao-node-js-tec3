9394de4baec6f5c211eeae8a292d26053ca9b432
Antonio A Alves <antonio.alves@duque.g12.br>
2025-07-02 16:45:55 -0300

    Gabarito da avalição de programação para web

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import Fastify from 'fastify'
+import { AppDataSource } from './data-source'
+import { usersRoutes } from './routes/users-routes'
+export async function startApp(){
+    const app = Fastify()
+    await AppDataSource.initialize().then(
+        ()=>{
+            console.log('Banco de 🎲 inicializado')
+        }
+    ).catch((ex)=>{
+        console.log('❌ Erro de conexão do banco de dados', ex)
+        process.exit(1)
+    })
+
+    // Registre as rotas e plugins aqui
+    app.register(usersRoutes)
+    return app
+}
